0960b14d244ccb3dc88d6f90f8857171488cdd44
McEmblem\ Files <nogglo>
2019-11-06 14:04:33 -0600

    e

--- Neil/js/main.js
+++ Neil/js/main.js
@@ -12,38 +12,40 @@ function wait(ms){
    }
  }
 
-for ( var i = 0; i < 100000; i++) {
-
+$("li").click(function() {
 
     console.log("grid", [].concat(...grid))
     checkwin(grid)
-    // if ($(this).hasClass("x") || $(this).hasClass("o")) return;
 
-    // let index = $(this).index()
+
+    if ($(this).hasClass("x") || $(this).hasClass("o")) return;
+
+    let index = $(this).index()
 
     // if (player === 0) {
 
-    let emptygrid = [].concat(...grid).map((x, idx) => {if (x === 0) {return idx}}).filter(e => e != null)
+    // let emptygrid = [].concat(...grid).map((x, idx) => {if (x === 0) {return idx}}).filter(e => e != null)
 
     // console.log("empty indexes", emptygrid)
 
-    let rplay = emptygrid[Math.floor(Math.random() * emptygrid.length)]
+    // let rplay = emptygrid[Math.floor(Math.random() * emptygrid.length)]
 
     // console.log("rplay", rplay)
 
-    // $($("li")[rplay]).addClass("x")
-    // player = 1;
-    grid[Math.floor(rplay / 3)][rplay % 3] = "x"
+    $($("li")[index]).addClass("x")
+    player = 1;
+    grid[Math.floor(index / 3)][index % 3] = "x"
+    // grid[Math.floor(rplay / 3)][rplay % 3] = "x"
 
     if ([].concat(...grid).indexOf(0) == -1) {
-        // alertin("Draw!")
+        alertin("Draw!")
         win2()
     }
 
     runalgo();
 
     // wait(1000);
-}
+});
 
 
 function checkwin(g) {
@@ -86,7 +88,7 @@ function checkitems(a, b, c) {
 }
 
 function win(p) {
-    // alertin(`Player ${p} Wins!`)
+    alertin(`Player ${p} Wins!`)
 
     win2()
 }
@@ -103,7 +105,7 @@ function win2() {
 }
 
 async function alertin(beep) {
-    // alert(beep)
+    alert(beep)
 }
 
 function runalgo() {
@@ -117,16 +119,6 @@ function runalgo() {
 
     for (let i in grid) {
         console.log("ran", grid[i])
-        if (counta(grid[i], "o") == 2 && grid[i].includes(0)) {
-            sel = [i, grid[i].indexOf(0)]
-            break
-        }
-
-        if (counta(rgrid[i], "o") == 2 && rgrid[i].includes(0)) {
-            sel = [rgrid[i].indexOf(0), i]
-            break
-        }
-
         //Player win checks
         if (counta(grid[i], "x") == 2 && grid[i].includes(0)) {
             sel = [i, grid[i].indexOf(0)]
@@ -139,14 +131,40 @@ function runalgo() {
         }
     }
 
+    for (let i in grid) {
+        if (counta(grid[i], "o") == 2 && grid[i].includes(0)) {
+            sel = [i, grid[i].indexOf(0)]
+            break
+        }
+
+        if (counta(rgrid[i], "o") == 2 && rgrid[i].includes(0)) {
+            sel = [rgrid[i].indexOf(0), i]
+            break
+        }
+    }
+
     if (sel.length == 0) {
         let diaglr = [grid[0][0], grid[1][1], grid[2][2]]
-        let diagrl = [grid[2][0], grid[1][1], grid[0][2]]
+        let diagrl = [grid[0][2], grid[1][1], grid[2][0]]
 
         if (counta(diaglr, "x") == 2 && diaglr.includes(0)) {
-            sel = [diaglr.indexOf(0), diaglr.indexOf(0)]
+            console.log("EEEEEE")
+            if (diaglr.indexOf(0) == 0) sel = [0, 0]
+            if (diaglr.indexOf(0) == 1) sel = [1, 1]
+            if (diaglr.indexOf(0) == 2) sel = [2, 2]
         }
         else if (counta(diagrl, "x") == 2 && diagrl.includes(0)) {
+            if (diagrl.indexOf(0) == 0) sel = [2, 0]
+            if (diagrl.indexOf(0) == 1) sel = [1, 1]
+            if (diagrl.indexOf(0) == 2) sel = [0, 2]
+        } 
+        else if (counta(diaglr, "o") == 2 && diaglr.includes(0)) {
+            console.log("EEEEEE")
+            if (diaglr.indexOf(0) == 0) sel = [0, 0]
+            if (diaglr.indexOf(0) == 1) sel = [1, 1]
+            if (diaglr.indexOf(0) == 2) sel = [2, 2]
+        }
+        else if (counta(diagrl, "o") == 2 && diagrl.includes(0)) {
             if (diagrl.indexOf(0) == 0) sel = [2, 0]
             if (diagrl.indexOf(0) == 1) sel = [1, 1]
             if (diagrl.indexOf(0) == 2) sel = [0, 2]
@@ -173,14 +191,15 @@ function runalgo() {
     //If there is no place to instantly block or win then the following code will run
     if (sel.length == 0) {
 
+        console.log("PENIS")
         //Corners
         if (grid[0][0] == 0) sel = [0, 0]
-        if (grid[0][2] == 0) sel = [0, 2]
-        if (grid[2][0] == 0) sel = [2, 0]
-        if (grid[2][2] == 0) sel = [2, 2]
+        else if (grid[0][2] == 0) sel = [0, 2]
+        else if (grid[2][0] == 0) sel = [2, 0]
+        else if (grid[2][2] == 0) sel = [2, 2]
 
         //Middle
-        if (grid[1][1] == 0) sel = [1, 1]
+        else if (grid[1][1] == 0) sel = [1, 1]
     }
     
     if (sel.length == 0) {
@@ -191,13 +210,13 @@ function runalgo() {
     console.log("sel", sel, parseInt(sel[0]) * 3 + parseInt(sel[1]))
     datacol.push(grid.concat(parseInt(sel[0]) * 3 + parseInt(sel[1])).join(","))
 
-    // $($("li")[sel[0] * 3 + parseInt(sel[1])]).addClass("o")
+    $($("li")[sel[0] * 3 + parseInt(sel[1])]).addClass("o")
     player = 0;
     grid[sel[0]][parseInt(sel[1])] = "o"
     checkwin(grid)
 
     if ([].concat(...grid).indexOf(0) == -1) {
-        // alertin("Draw!")
+        alertin("Draw!")
         win2()
     } 
 }
